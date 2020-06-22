81433f64f273ccd600a635354593a57c021a18d6
Karl Hallsby <karl@hallsby.com>
2020-06-22 23:11:20 +0200

    Macros can be "overloaded" in their definition, and have different bodies

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -1,12 +1,14 @@
 // macros4.rs
 // Make me compile! Execute `rustlings hint macros4` for hints :)
 
-// I AM NOT DONE
+/* If you create an overloaded macro (one that can take different types of arguments
+ * which all share the same name), then you must have a separator between the
+ * alternative macro bodies. */
 
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    };
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
     }
