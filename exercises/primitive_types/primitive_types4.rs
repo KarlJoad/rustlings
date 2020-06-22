a99267b7063f85e21653bc8d1802c7482af44b9c
Karl Hallsby <karl@hallsby.com>
2020-06-22 22:27:48 +0200

    Slices take a reference to a thing and the range, which is [x..y]
    The range has a non-inclusive end, meaning in interval notation, the
    slice contains elements from [x, x+1, x+2, ..., y-1, y).
    Namely, everything starting from x, until (and NOT including) y.

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -2,13 +2,12 @@
 // Get a slice out of Array a where the ??? is so that the test passes.
 // Execute `rustlings hint primitive_types4` for hints!!
 
-// I AM NOT DONE
-
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    let nice_slice = &a[1..4]; // Uses similar syntax as Haskell
+    // 1..4 => [1, 4), non-inclusive end. Arrays start index at 0.
 
     assert_eq!([2, 3, 4], nice_slice)
 }
