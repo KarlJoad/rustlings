d82b8d6c1930b2329bbdd5275d3c2b268327a533
Karl Hallsby <karl@hallsby.com>
2020-06-22 23:19:49 +0200

    Box allows us to have multiple kinds of things
    In reality, Box provides us a "box" on the stack that points to
    something on the heap. This thing on the heap is allowed to be
    relatively unsized compared to the stack, so we can have different
    types of Errors pointed to.
    
    This is useful here because the ? operator will return and end
    execution early if an Error condition is generated. In that case, then
    the Box is utilized and the Error is returned to the calling function.
    
    However, if we proceed without error, the Result's Ok variant contains
    the end value.

--- exercises/error_handling/errorsn.rs
+++ exercises/error_handling/errorsn.rs
@@ -17,19 +17,20 @@
 //
 // Execute `rustlings hint errorsn` for hints :)
 
-// I AM NOT DONE
+/* Using Box, we can create a Box that contains one of several possible errors.
+ * These errors are constrained to be elements of the Error type class. */
 
 use std::error;
 use std::fmt;
 use std::io;
 
 // PositiveNonzeroInteger is a struct defined below the tests.
-fn read_and_validate(b: &mut dyn io::BufRead) -> Result<PositiveNonzeroInteger, ???> {
+fn read_and_validate(b: &mut dyn io::BufRead) -> Result<PositiveNonzeroInteger, Box<dyn error::Error>> {
     let mut line = String::new();
-    b.read_line(&mut line);
-    let num: i64 = line.trim().parse();
+    b.read_line(&mut line)?;
+    let num: i64 = line.trim().parse()?;
     let answer = PositiveNonzeroInteger::new(num);
-    answer
+    Ok(answer?)
 }
 
 //
