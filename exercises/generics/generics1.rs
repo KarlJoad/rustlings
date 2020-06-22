863359c5018f02efd17bd0ad67a16ae95d760810
Karl Hallsby <karl@hallsby.com>
2020-06-22 23:52:22 +0200

    The shopping list should be a generic vector until first assigned

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -1,10 +1,12 @@
-// This shopping list program isn't compiling! 
+// This shopping list program isn't compiling!
 // Use your knowledge of generics to fix it.
 
-// I AM NOT DONE
+/* I feel like this may be the solution, but the _ gives little constraint on
+ * what may be present in the list. If there were additional items, we could
+ * constrain the possible types available in the list more.
+ * However, with the current usage, a Vec<&str> is appropriate.*/
 
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
+    let mut shopping_list: Vec<_> = Vec::new();
     shopping_list.push("milk");
 }
-
