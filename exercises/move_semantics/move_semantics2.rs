b58793d077e4faedf466dfa91e7d6bc35fd56329
Karl Hallsby <karl@hallsby.com>
2020-06-22 23:45:28 +0200

    The fill_vec fuunction here needs to borrow the vector here
    Because the vector is used later, within the main function, the vector
    cannot be transferred to the fill_vec function.
    Because of this, the function must create a NEW vector that has the
    values pushed on and then returned.
    
    When RETURNING something, ownership is TRANSFERRED. Meaning that when
    the fill_vec returns, the newvec to main, **main** owns the newvec as
    vec1.
    If something is defined to be mutable, ANY and ALL other aliases for
    that thing must ALSO be labeled as mutable.

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,12 +2,14 @@
 // Make me compile without changing line 13!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
+/* Here, fill_vec borrows vec0, and creates a new vector that will be owned by main.
+ * This way, both vec0 and vec1 exist at the same time, but vec1 is somewhat independent
+ * of vec0. */
 
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(&vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -17,12 +19,12 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    let mut newvec = Vec::new();
 
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
+    newvec.push(22);
+    newvec.push(44);
+    newvec.push(66);
 
-    vec
+    newvec
 }
