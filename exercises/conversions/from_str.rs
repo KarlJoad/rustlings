ced7fda76cd7709778c7101514931078dcd879ed
Karl Hallsby <karl@hallsby.com>
2020-06-23 00:06:45 +0200

    FromStr is very similar to From<&str>

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -10,7 +10,6 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
 // Steps:
 // 1. If the length of the provided string is 0, then return an error
 // 2. Split the given string on the commas present in it
@@ -23,6 +22,21 @@ struct Person {
 impl FromStr for Person {
     type Err = String;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+	if s.len() == 0 {
+	    return Err("Provided a string of length 0. Fail.".to_string());
+	}
+
+	let ss: Vec<&str> = s.split(",").collect();
+	if ss.len() < 2 ||
+	    ss[0] == "" || ss[1] == "" {
+	    return Err("One of the provided fields was empty.".to_string());
+	} else {
+	    if let Ok(inputage) = ss[1].parse::<usize>() {
+		return Ok(Person {name: ss[0].to_string(), age: inputage});
+	    } else {
+		return Err("You provided an age that is not a number.".to_string());
+	    }
+	}
     }
 }
 
