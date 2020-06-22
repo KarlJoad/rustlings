49865cbb449c40789d4c525e0ce21533ea64fc27
Karl Hallsby <karl@hallsby.com>
2020-06-22 23:10:56 +0200

    Macros are always private in a module. Must EXPOSE macros to others

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -2,8 +2,10 @@
 // Make me compile, without taking the macro out of the module!
 // Execute `rustlings hint macros3` for hints :)
 
-// I AM NOT DONE
+/* If a macro is defined within a module, you cannot use normal function namespacing
+ * rules. You have to declare that macros can be used within the module.*/
 
+#[macro_use]
 mod macros {
     macro_rules! my_macro {
         () => {
