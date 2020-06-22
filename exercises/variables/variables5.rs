81e2c3ccf6e006327d51e27aec99cfac772eddaa
Karl Hallsby <karl@hallsby.com>
2020-06-22 22:16:58 +0200

    Rust allows for shadowing variables with the same name

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,12 @@
 // variables5.rs
 // Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
 
-// I AM NOT DONE
-
+/* A let binding can shadow outer/earlier bindings. Thus, the second number, the
+ * one that has the value of the integer number 3 is the only one available after
+ * that line, and the number with the value of a string 3 is no longer available. */
 fn main() {
     let number = "3"; // don't change this line
     println!("Number {}", number);
-    number = 3;
+    let number = 3;
     println!("Number {}", number);
 }
