81a605cf84525cc45d249ee5de74eeb1fe688159
Karl Hallsby <karl@hallsby.com>
2020-06-22 22:13:45 +0200

    By default, all variables are immutable. State mutability explicitly

--- exercises/variables/variables3.rs
+++ exercises/variables/variables3.rs
@@ -1,10 +1,12 @@
 // variables3.rs
 // Make me compile! Execute the command `rustlings hint variables3` if you want a hint :)
 
-// I AM NOT DONE
+/* By default, all variables behave like in functional programming languages,
+ * where the value of the variable is immutable. We must explicitly state that
+ * the variable is mutable to change it after its initial binding. */
 
 fn main() {
-    let x = 3;
+    let mut x: u32 = 3;
     println!("Number {}", x);
     x = 5; // don't change this line
     println!("Number {}", x);
