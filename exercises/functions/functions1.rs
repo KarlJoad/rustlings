d7d7070c4cad2fad3d89af23c99e665b4c1ec189
Karl Hallsby <karl@hallsby.com>
2020-06-22 22:20:41 +0200

    Create useless function that takes nothing in and returns nothing

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,7 +1,9 @@
 // functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
+pub fn call_me() -> () {
+    return (); // This is a function that takes nothing in and returns nothing
+}
 
 fn main() {
     call_me();
