72c9dee7728bc16dec6744d9ba66cee581f2c56c
Karl Hallsby <karl@hallsby.com>
2020-06-22 23:17:18 +0200

    Functions that use ? MUST have a supported return type
    This return type must yield something that can potentially yield
    something of type Error.
    This means that in this example, the main function can potentially
    return an Ok that wraps either a String or an Err that wraps a
    ParseIntError.
    
    Because the ? operator requires the function to return something of
    those types, and the last expression of a function is ALSO its return
    type, the main function must be modified here to format a String
    output that is the same thing as the original println!.

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -4,21 +4,24 @@
 // Why not? What should we do to fix it?
 // Execute `rustlings hint errors3` for hints!
 
-// I AM NOT DONE
+/* The ? operator will return erroneous conditions early.
+ * This means that if total_cost returns an error, then main will return the error
+ * immediately. However, if it is valid, then we only need to consider the correctly
+ * functioning condition and how to handle it. */
 
 use std::num::ParseIntError;
 
-fn main() {
+fn main() -> Result<String, ParseIntError> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
     let cost = total_cost(pretend_user_input)?;
 
     if cost > tokens {
-        println!("You can't afford that many!");
+        Ok(format!("You can't afford that many!"))
     } else {
         tokens -= cost;
-        println!("You now have {} tokens.", tokens);
+        Ok(format!("You now have {} tokens.", tokens))
     }
 }
 
